02969f5cf4b36fd1c4e701ea33a301231f23b442
TemaGarfield <49874332+TemaGarfield@users.noreply.github.com>
2020-08-06 18:36:46 +0300

    Add files via upload

--- Basics/Task1/src/by/kotik/task1/main/Main.java
+++ Basics/Task1/src/by/kotik/task1/main/Main.java
@@ -2,6 +2,10 @@ package by.kotik.task1.main;
 
 import by.kotik.task1.logic.Test1;
 
+/*
+ * 1. Создайте класс Test1 двумя переменными. Добавьте метод вывода на экран и методы изменения этих переменных. 
+ * Добавьте метод, который находит сумму значений этих переменных, и метод, который находит наибольшее значение из этих двух переменных.*/
+
 public class Main {
 	public static void main(String[] args) {
 	    Test1 test = new Test1(11, 10);
